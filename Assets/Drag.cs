9475d6eb8cfcf8cd83a60e9acf4dd651c1df39c1
Graham Pentheny <graham.pentheny@gmail.com>
2015-02-18 18:26:34 -0500

    Initial commit

---
+++ Assets/Drag.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Drag : MonoBehaviour, IPointerDownHandler, IDragHandler {
+	public RectTransform panelRectTransform;
+	
+	private Vector2 originalLocalPointerPosition;
+	private Vector3 originalPanelLocalPosition;
+	private RectTransform parentRectTransform;
+	
+	void Awake () {
+		parentRectTransform = panelRectTransform.parent as RectTransform;
+	}
+	
+	public void OnPointerDown (PointerEventData data) {
+		originalPanelLocalPosition = panelRectTransform.localPosition;
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, data.position, data.pressEventCamera, out originalLocalPointerPosition);
+	}
+	
+	public void OnDrag (PointerEventData data) {
+		if (panelRectTransform == null || parentRectTransform == null)
+			return;
+		
+		Vector2 localPointerPosition;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, data.position, data.pressEventCamera, out localPointerPosition)) {
+			Vector3 offsetToOriginal = localPointerPosition - originalLocalPointerPosition;
+			panelRectTransform.localPosition = originalPanelLocalPosition + offsetToOriginal;
+		}
+	}
+}
