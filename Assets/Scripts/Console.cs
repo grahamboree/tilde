9a15899141206496214fc944e63eaa67436159ec
Graham Pentheny <graham.pentheny@gmail.com>
2015-03-10 17:55:49 -0400

    Cleaned up console code, general help shows docstring previews.

--- Assets/Scripts/Console.cs
+++ Assets/Scripts/Console.cs
@@ -12,10 +12,7 @@ public class ConsoleCommand : Attribute {
 	public string commandName;
 	public string docstring;
 
-	public ConsoleCommand() {
-	}
-
-	public ConsoleCommand(string docstring) {
+	public ConsoleCommand(string docstring = null) {
 		this.docstring = docstring;
 	}
 
@@ -27,40 +24,46 @@ public class ConsoleCommand : Attribute {
 
 public class Console {
 	#region Fields.
-	// Command history.
+	/// The complete console command execution history.
 	public ConsoleHistory history = new ConsoleHistory();
 
-	// Console output.
+	/// Callback type for Console.Changed events.
 	public delegate void onChangeCallback(string logText);
+
+	/// Occurs when the log contents have changed, most often occurring when a command is executed.
 	public event onChangeCallback Changed;
 
 	// Registering console commands
-	public delegate string commandAction(params string[] args);
-	public delegate string simpleCommandAction();
-	public delegate void silentCommandAction(string[] args);
-	public delegate void simpleSilentCommandAction();
+	private delegate string commandAction(params string[] args);
+	private delegate string simpleCommandAction();
+	private delegate void silentCommandAction(string[] args);
+	private delegate void simpleSilentCommandAction();
+
 	private class CommandEntry {
 		public string docs;
 		public commandAction action;
 	}
-	Dictionary<string, CommandEntry> commandMap = new Dictionary<string, CommandEntry>();
+
+	private Dictionary<string, CommandEntry> commandMap = new Dictionary<string, CommandEntry>();
 
 	// Log scrollback.
-	StringBuilder logContent = new StringBuilder();
+	private StringBuilder logContent = new StringBuilder();
 
-	string logMessageColor = "586e75";
-	string warningMessageColor = "b58900";
-	string errorMessageColor = "dc322f";
+	/// The full console log string.
+	public string Content { get { return logContent.ToString(); } }
+
+	// Log styling.
+	private string logMessageColor = "586e75";
+	private string warningMessageColor = "b58900";
+	private string errorMessageColor = "dc322f";
 	#endregion
 	
 	#region Singleton
 	public static Console instance { get { return _instance; } }
-
 	private static Console _instance = new Console();
-
 	private Console() {
 		// Listen for Debug.Log calls.
-		Application.RegisterLogCallback(Log);
+		Application.logMessageReceived += Log;
 		commandMap["help"] = new CommandEntry(){docs = "View available commands as well as their documentation.", action = Help};
 		commandMap["res"] = new CommandEntry(){docs = "List supported fullscreen resolutions on this device", action = _ => SupportedResolutions()};
 		FindCommands();
@@ -68,34 +71,50 @@ public class Console {
 	#endregion
 
 	#region Public Methods.
+	/// <summary>
+	/// Print a string to the console window.  Appears as if it was command output.
+	/// </summary>
+	/// <param name="message">The text to print.</param>
 	public void OutputStringToConsole(string message) {
 		logContent.Append("\n");
 		logContent.Append(message);
 		Changed(logContent.ToString());
 	}
 
+	/// <summary>
+	/// Run a console command.  This is used by the text input UI to parse and execute commands.
+	/// </summary>
+	/// <param name="command">The complete command string, including any arguments.</param>
 	public void RunCommand(string command) {
 		logContent.Append("\n> ");
 		logContent.Append(command);
+		// Inform the UI that the console text has changed so it can redraw, 
+		// in case the command takes a while to execute.
 		Changed(logContent.ToString());
 		history.AddCommandToHistory(command);
-		SilentlyRunCommand(command);
+		logContent.Append("\n");
+		logContent.Append(SilentlyRunCommand(command));
 		Changed(logContent.ToString());
 	}
 
-	public void SilentlyRunCommand(string commandString) {
+	/// <summary>
+	/// Execute a console command, but do not display output in the console window.
+	/// </summary>
+	/// <param name="commandString">The complete command string, including any arguments.</param>
+	public string SilentlyRunCommand(string commandString) {
 		string[] splitCommand = commandString.Split(' ');
 		string commandName = splitCommand[0];
 		CommandEntry command = null;
 		if (commandMap.TryGetValue(commandName, out command)) {
 			try {
-				LogMessage(command.action(splitCommand.Skip(1).ToArray()));
+				return command.action(splitCommand.Skip(1).ToArray());
 			} catch (Exception e) {
 				LogError(e.Message);
 			}
 		} else {
 			LogError("Unknown console command: " + commandName);
 		}
+		return "";
 	}
 
 	public string Autocomplete(string partialCommand) {
@@ -114,7 +133,29 @@ public class Console {
 			string result = "Available commands:\n";
 			string[] commands = commandMap.Keys.ToArray();
 			Array.Sort(commands);
-			return result + String.Join("\n", commands.Select(x => "\t" + x).ToArray());
+			int maxCommandLength = commands.Select(x => x.Length).Max();
+			foreach (string c in commands) {
+				result += "\n  " + c + new string(' ', (maxCommandLength - c.Length) + 3);
+				string docstring = commandMap[c].docs;
+				int docstringLength = 80; // starting line length
+				docstringLength -= 2; // Leftmost indent
+				docstringLength -= maxCommandLength; // command name length
+				docstringLength -= 3; // rightmost padding between command and docstring.
+				docstringLength = Mathf.Max(docstringLength, 0);
+
+				bool addElipsis = docstringLength < docstring.Length;
+				if (addElipsis) {
+					docstringLength -= 3; // elipsis
+				}
+
+				result += new string(docstring.Take(docstringLength).ToArray());
+				if (addElipsis) {
+					result += "...";
+				}
+			}
+			result += "\n" + new string(' ', 80) + "|";
+			//return result + String.Join("\n", commands.Select(x => "\t" + x).ToArray());
+			return result;// + String.Join("\n", commandMap.Select(x => "\t" + x.Key + "\t" + new string(x.Value.docs.Take(60).ToArray())).ToArray());
 		}
 		
 		CommandEntry command = null;
